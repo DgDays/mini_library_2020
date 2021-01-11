e8f8274d67a641bbf921c6d6ccd18a02cfa6f9a9
DGDays <oleg.pariev@gmail.com>
2021-01-11 08:21:23 +0500

    Перезалил снова с electron_interf

--- Клиент библиотекарь/src/index.js
+++ Клиент библиотекарь/src/index.js
@@ -1,48 +1,53 @@
-const { app, BrowserWindow } = require('electron');
-const os = require('os-utils');
-const path = require('path');
+//main.js
+//Основная конфигуация для старта приложения
+const electron = require('electron');
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
+const app = electron.app;
 
-const createWindow = () => {
+const BrowserWindow = electron.BrowserWindow;
+// отправка отчёта об ошибках, url укажем в будущем
+const { crashReporter } = require('electron')
+
+crashReporter.start({  compress: true, submitURL: '' })
+
+let mainWindow;
+
+
+function createWindow () {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
+    titleBarStyle: 'hidden',
     width: 1200,
     height: 800,
-    icon: __dirname + '/icon.ico',
-    webPreferences: {
-      nodeIntegration: true
-    }
+    minWidth: 450,
+    minHeight: 650,
+    icon: __dirname + '/icon.ico'
+    //fullscreen:true,
+
+    }); //основная конфигуация
+
+
+  mainWindow.loadURL('file://' + __dirname + '/index.html'); //загрузка html файла
+
+ 
+  mainWindow.on('closed', function() {
+    mainWindow = null;
   });
+} //закрытие главного окна
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-};
+app.on('ready', createWindow); //создание окна при готовности приложения
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
+app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit();
   }
-});
+});  //закрытие окна и сворачивание в док если это OS X
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
+app.on('activate', function () {
+
+  if (mainWindow === null) {
     createWindow();
-  }
-});
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+  }
+}); //восстановление окна
+
