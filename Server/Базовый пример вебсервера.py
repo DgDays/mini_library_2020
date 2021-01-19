5f6206eeec29e6929efa710c8f38d4a6a658ad83
DGDays <oleg.pariev@gmail.com>
2021-01-19 08:25:17 +0500

    Добавил комменты

--- Server/Базовый пример вебсервера.py
+++ Server/Базовый пример вебсервера.py
@@ -2,25 +2,24 @@
 # WS Сервер (базовый пример)
 
 import asyncio # Библиотека стандартной архитектуры асинхронного ввода - вывода в Python
-import websockets
-import pymysql
-import json
-import datetime
+import websockets # Библиотека вебсокетов
+import pymysql    # Библиотека для обращения к MySQL
+import json       # Библиотека для работы с json
+import datetime   # Библиотека для корректного преобразования данных в DD.MM.YYYY
 
 async def hello(websocket, path): # На стороне сервера websocket выполняет 
     # сопрограмму обработчика hello один раз для каждого соединения
-    ask = await websocket.recv()
-    ask = json.loads(ask)
-    if ask["comm"] == 'login':
+    ask = await websocket.recv() # Получение данных с клиента
+    ask = json.loads(ask)        # Чтение  json
+    if ask["comm"] == 'login':   # Проверка комманды. Если comm == login запуск регистрации
         con = pymysql.connect(host='92.49.138.74', user='DGDays', 
             password='669202Qazwerty+', db='library')
-        with con:
+        with con:                # Подключение к Мускулам
             cur = con.cursor()
-            print(ask)
-            cur.execute('''SELECT * FROM users WHERE Login=(%s) AND Password=(%s)''',(ask['login'],ask['password']))
-            greeting = cur.fetchone()
-            if greeting != None:
-                greeting = {
+            cur.execute('''SELECT * FROM users WHERE Login=(%s) AND Password=(%s)''',(ask['login'],ask['password'])) # Получение данных пользователя под этим логином и паролем
+            greeting = cur.fetchone() # Получение одной единственной записи аккаунта
+            if greeting != None: # Проверка ответа MySQL. Если не None, то создаёт json с данными пользователя и ответом сервера Good
+                greeting = {     # Сам json с данными юзера
                     "id": greeting[0],
                     "Login": greeting[1],
                     "Password": greeting[2],
@@ -30,15 +29,14 @@ async def hello(websocket, path): # На стороне сервера websocket
                     'res': "Good"
                 }
             else:
-                greeting = {'res': 'None'}
-            greeting = json.dumps(greeting)
-            print(greeting)
-            await websocket.send(greeting)
+                greeting = {'res': 'None'} # Если ответ MySQL None, то отправляет ответ сервера None
+            greeting = json.dumps(greeting) # Преобразование в json для отправки данных
+            await websocket.send(greeting) # Отправка json в клиент, который обратился
 
-start_server = websockets.serve(hello, "localhost", 8765)
+start_server = websockets.serve(hello, "localhost", 8765) # Старт сервака
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+asyncio.get_event_loop().run_until_complete(start_server) # Асинхронный запуск до тех пор, пока сервак не заработает
+asyncio.get_event_loop().run_forever()                    # Запускает петлю работы сервака
 # Закрывает соединение после возврата
 # async/await -(специальный синтаксис для работы с промисами)
 # Промис- это объект,
