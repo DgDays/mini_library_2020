0b02003f53d375ca9f8ded490e23f11685462f90
zakhar24 <53836202+zakhar24@users.noreply.github.com>
2021-01-24 22:21:57 +0500

    фронт изменил

--- Клиент библиотекарь/src/main.js
+++ Клиент библиотекарь/src/main.js
@@ -1,41 +1,42 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const { app, BrowserWindow } = require('electron')
+// const path = require("path")
+// const url = require("url")
 
 //создаём окно
 function createWindow() {
-  let appWindow = new BrowserWindow({
+  let mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
     minWidth: 450,
     minHeight: 650,
-    blur: true,
     autoHideMenuBar: true,
     icon: __dirname + '/icon.ico'
   });
   let child = new BrowserWindow({
     width: 800,
     height: 700,
-    parent: appWindow,
+    parent: mainWindow,
     // closable: false,
     modal: true,
     autoHideMenuBar: true, 
     icon: __dirname + '/icon.ico',
   });
   child.loadFile("src/login.html");
-  child.once('ready-to-show', () => {
-    child.show()
-  })
+  // child.once('ready-to-show', () => {
+  //   child.show()
+  // })
   // appWindow.webContents.openDevTools();
-  appWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 };
 
 app.on('ready', () => {
   createWindow();
 });
 
-app.on('window-all-closed', function() {
-  app.quit();
+app.on('window-all-closed', () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 // // отправка отчёта об ошибках, url укажем в будущем
