9415bce6fde16b31bffe626164aebd091edfc278
DGDays <oleg.pariev@gmail.com>
2021-01-31 18:57:22 +0500

    Разделил клиенты

---
+++ Клиент читатель(ПК пока XD)/src/main.js
@@ -0,0 +1,44 @@
+const { app, BrowserWindow } = require('electron')
+// const path = require("path")
+// const url = require("url")
+
+//создаём окно
+function createWindow() {
+  let mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 450,
+    minHeight: 650,
+    autoHideMenuBar: true,
+    icon: __dirname + '/icon.ico'
+  });
+  let child = new BrowserWindow({
+    width: 800,
+    height: 700,
+    parent: mainWindow,
+    // closable: false,
+    modal: true,
+    autoHideMenuBar: true, 
+    icon: __dirname + '/icon.ico',
+  });
+  child.loadFile("src/login.html");
+  // child.once('ready-to-show', () => {
+  //   child.show()
+  // })
+  // appWindow.webContents.openDevTools();
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+};
+
+app.on('ready', () => {
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+// // отправка отчёта об ошибках, url укажем в будущем
+// const { crashReporter, globalShortcut } = require('electron')
+// crashReporter.start({  compress: true, submitURL: '' })
