47e9e17a4d30bf4154292b3c6e2091b5c12218db
mini-lib <perchik03300@gmail.com>
2021-11-06 01:22:20 +0500

    Добавил блюр

--- client_reader/src/index.js
+++ client_reader/src/index.js
@@ -1,5 +1,13 @@
 const ipc = require('electron').ipcRenderer
 
+// функция для изменения стиля
+function changeStyleDiv()   {
+    document.getElementById('body').style.filter = 'blur(0px)';
+}
+
 ipc.on('login', function (event, arg){
     console.log(arg);
+    if (arg.res == "Good") {
+        changeStyleDiv();
+    }
 })
