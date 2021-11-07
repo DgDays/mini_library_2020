3a3ca3c6466a6f4486faa6060840e73ead675ea4
DGDays (Олег) <oleg.pariev@gmail.com>
2021-11-07 19:42:11 +0500

    Добавил пример работы добавления ip на github

--- server/github_demo.py
+++ server/github_demo.py
@@ -1,5 +1,7 @@
 from github import Github
 from base64 import b64decode
+import json 
+from types import SimpleNamespace
 
 # using an access token
 g = Github("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")
@@ -9,11 +11,17 @@ for repo in g.get_user().get_repos():
         repo = repo
         break
 
-ip = ''
+ip = '192.168.1.146'
 city = ''
 
-contents = repo.get_contents("ip-list.txt", ref="main")
-file_content = str(b64decode(contents.content))[2:-1]
-file_content = file_content + f'|{ip}|{city}|'
-#file_content.replace('||', '|').split('|')[1:-1]
-repo.update_file(contents.path, "more", file_content, contents.sha, branch="main")
+contents = repo.get_contents("ip-list.json", ref="main")
+print(str(b64decode(contents.content))[2:-1])
+file_content = json.loads(str(b64decode(contents.content))[2:-1])
+last = f"{int(list(file_content.keys())[-1])+1}"
+print(last)
+file_content.update({last : ''})
+file_content[last] = {
+    'IP' : '192.168.1.146',
+    'City' : 'Novotroitsk'
+}
+repo.update_file(contents.path, "more", json.dumps(file_content), contents.sha, branch="main")
