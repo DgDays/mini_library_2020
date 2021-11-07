63870c8618da13dd978cfad7b9ee5029ff9f924f
mini-lib <perchik03300@gmail.com>
2021-11-07 20:30:13 +0500

    Добавил выпадающее меню

--- client_reader/src/index.js
+++ client_reader/src/index.js
@@ -13,3 +13,22 @@ ipc.on('login', function (event, arg){
         }
     }, 20)
 })
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.project-btn-more')) {
+  
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
